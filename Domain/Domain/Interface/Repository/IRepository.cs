db90fe721709c89030a82459d26f9be964286ae0
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2024-04-23 01:19:20 -0300

    fix: removendo métodos inutilizados

--- Domain/Domain/Interface/Repository/IRepository.cs
+++ Domain/Domain/Interface/Repository/IRepository.cs
@@ -8,18 +8,6 @@ namespace Domain.Interface.Repository;
 public interface IRepository<T> where T : class
 {
     Task AddAsync(T entity);
-
-    Task AddRangeAsync(IList<T> entity);
-
-    Task UpdateAsync(T entity);
-
-    Task UpdateRangeAsync(IList<T> entity);
-
-    Task DeleteAsync(T entity);
-
-    Task DeleteRangeAsync(IList<T> entity);
-
-    Task<T> Get(Expression<Func<T, bool>> expression);
-
+    Task<IList<T>> GetAll();
     Task<T> GetById(int key);
 }
--- Infrastructure/Repository/Repository.cs
+++ Infrastructure/Repository/Repository.cs
@@ -12,27 +12,9 @@ public class Repository<T>(FaturaContext context) : IRepository<T> where T : cla
     public async Task AddAsync(T entity) => 
         await _context.Set<T>().AddAsync(entity);
 
-    public async Task AddRangeAsync(IList<T> entity) => 
-        await _context.Set<T>().AddRangeAsync(entity);
-
-    public async Task DeleteAsync(T entity) => 
-        await Task.Run(() => { _context.Set<T>().Remove(entity); });
-
-    public async Task DeleteRangeAsync(IList<T> entity) => 
-        await Task.Run(() => { _context.Set<T>().RemoveRange(entity); });
-
-    public async Task UpdateAsync(T entity) => 
-        await Task.Run(() => { _context.Set<T>().Update(entity); });
-
-    public async Task UpdateRangeAsync(IList<T> entity) => 
-        await Task.Run(() => { _context.Set<T>().UpdateRange(entity); });
-
-    public async Task<T> Get(Expression<Func<T, bool>> expression) =>
-        await _context.Set<T>().Where(expression).FirstOrDefaultAsync();
-
-    public async Task<IEnumerable<T>> GetAll() =>
+    public async Task<IList<T>> GetAll() =>
         await _context.Set<T>().ToListAsync();
 
-    public async Task<T?> GetById(int key) =>
+    public async Task<T> GetById(int key) =>
         await _context.Set<T>().FindAsync(key);
 }
